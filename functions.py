d5fcc8b85fa9e97bf2d76e458b0390bfcf0db01f
raxov99 <45939979+raxov99@users.noreply.github.com>
2022-12-18 13:02:15 +0100

    Add files via upload

--- functions.py
+++ functions.py
@@ -1,10 +1,16 @@
 import matplotlib.pyplot as plt
 import json
+import os
 from scipy.optimize import curve_fit
 from instruments import *
 from datetime import date
 
 N = 100
+ch_AWG, ch_SMU = 1, 2
+ch_DAQ_AWG, ch_DAQ_SMU = 113, 111
+read_V = .1
+V_lim = [2.0, 2.6, -2.4, -3.0]
+ss = 1.2
 
 instruments = {}
 rm = pyvisa.ResourceManager()
@@ -14,8 +20,8 @@ for r in lr:
         instruments['AWG'] = ArbitraryWaveformGenerator(r)
     elif "daq" in r:
         instruments['DAQ'] = DataAcquisition(r)
-    #elif "smu" in r:
-    elif "172.31.182.32" in r:
+    #elif "172.31.182.32" in r:
+    elif "smu" in r:
         instruments['SMU'] = SourceMeasureUnit(r)
     elif "osc" in r:
         instruments['OSC'] = Oscilloscope(r)
@@ -32,26 +38,26 @@ def reset(instruments):
     for key in instruments.keys():
         instruments[key].reset()
 
-def my_wf(V, n, T, W, read=False, read_V=.1):
-    SRAT = 10/W if W else 1
+def my_wf(V, n, W, T, read, read_V):
+    SRAT = 10/np.array(W).min() if np.array(W).min() > 0 else 1
+    W_len = np.round(np.array(W)*SRAT).astype(int)
     T_len = round(T*SRAT)
-    W_len = round(W*SRAT)
-    if not len(V) == len(n):
-        raise RuntimeError("Inputs V and n of my_wf have different lengths.")
+    if not len(V) == len(n) == len(W):
+        raise RuntimeError("Inputs V, n, and W of my_wf have different lengths.")
     for i in range(len(V)):
         if i == 0:
-            n_read = T_len-W_len
+            n_read = T_len-W_len[i]
             n_zeros = T_len//2
             wf = [0 for j in range(n_zeros)] + [(read_V if read else 0) for j in range(n_read)]
         for j in range(n[i]):
-            wf += [V[i] for k in range(W_len)]
-            wf += [(read_V if read else 0) for k in range(T_len - W_len)]
+            wf += [V[i] for k in range(W_len[i])]
+            wf += [(read_V if read else 0) for k in range(T_len - W_len[i])]
     wf += [0 for j in range(n_zeros)]
     return wf, SRAT
 
 def get_and_set_wf(key, wf_dict, aint=False):
     inst = instruments[key]
-    wf, SRAT = my_wf(wf_dict['V'], wf_dict['n'], wf_dict['T'], wf_dict['W'], wf_dict['read'], wf_dict['read_V'])
+    wf, SRAT = my_wf(wf_dict['V'], wf_dict['n'], wf_dict['W'], wf_dict['T'], wf_dict['read'], wf_dict['read_V'])
     if aint:
         inst.set_wf(wf, SRAT, wf_dict['ch'], aint=aint)
     else:
@@ -107,7 +113,7 @@ def get_R_mean(t, V_i, read_V, R, valid, T_len, W_len, n):
     R_mean = np.array(R_mean)   
     return R_mean, a, b
 
-def meas_AWG(ch_DAQ, wf_dict, chs_OSC, R_s=1e3, R_min=5e3, plot=True, save=True):
+def meas_AWG(ch_DAQ, wf_dict, chs_OSC, R_s=1e3, R_min=5e3, plot=True, save=False):
     ch_AWG = wf_dict['ch']
     
     DAQ = instruments['DAQ']
@@ -132,6 +138,8 @@ def meas_AWG(ch_DAQ, wf_dict, chs_OSC, R_s=1e3, R_min=5e3, plot=True, save=True)
     AWG.set_outp(ch_AWG, 0)
     
     wf = []
+    if 'wf' not in os.listdir():
+        os.mkdir('wf')
     for ch_OSC in chs_OSC:
         path = f"wf/{ch_OSC}.csv"
         with open(path, "w") as f:
@@ -139,7 +147,7 @@ def meas_AWG(ch_DAQ, wf_dict, chs_OSC, R_s=1e3, R_min=5e3, plot=True, save=True)
         wf.append(np.transpose(np.loadtxt(path, delimiter=";")))
     
     T_len = round(T*SRAT)
-    W_len = round(W*SRAT)
+    W_len = np.round(np.array(W)*SRAT).astype(int).min()
     
     if read and read_V > 0:
         R = R_s*(wf[0][1]-wf[1][1])/wf[1][1]
@@ -172,7 +180,7 @@ def meas_AWG(ch_DAQ, wf_dict, chs_OSC, R_s=1e3, R_min=5e3, plot=True, save=True)
         
     return results
 
-def meas_SMU(ch_DAQ, wf_dict, chs_OSC, plot=True, save=True):
+def meas_SMU(ch_DAQ, wf_dict, chs_OSC, plot=True, save=False):
     T = wf_dict['T']
     W = wf_dict['W']
     n = wf_dict['n']
@@ -186,7 +194,7 @@ def meas_SMU(ch_DAQ, wf_dict, chs_OSC, plot=True, save=True):
     instruments['OSC'].configure(SRAT, t_scale, t_pos, chs_OSC, scales, trig_source, trig_level)
     return meas_SMU_(ch_DAQ, wf_dict, plot=plot, save=save)
 
-def meas_SMU_(ch_DAQ, wf_dict, plot=True, save=True):
+def meas_SMU_(ch_DAQ, wf_dict, plot=True, save=False):
     ch_SMU = wf_dict['ch']
     DAQ = instruments['DAQ']
     SMU = instruments['SMU']
@@ -207,7 +215,7 @@ def meas_SMU_(ch_DAQ, wf_dict, plot=True, save=True):
     read = wf_dict['read']
     SRAT = SMU.get_srat(ch_SMU)
     T_len = round(T*SRAT)
-    W_len = round(W*SRAT)
+    W_len = np.round(np.array(W)*SRAT).astype(int).max()
     
     aint = SMU.get_trig_source(ch_SMU) == 'AINT'
     if read:
@@ -250,7 +258,7 @@ def meas_SMU_(ch_DAQ, wf_dict, plot=True, save=True):
     
     return results
 
-def meas_AWG_SMU(wf_dict_AWG_list, N, ch_DAQ_AWG, ch_DAQ_SMU, wf_dict_SMU, R_s = 1e3, plot=True, save=True):
+def meas_AWG_SMU(wf_dict_AWG_list, N, ch_DAQ_AWG, ch_DAQ_SMU, wf_dict_SMU, R_s = 1e3, plot=True, save=False):
     if not len(N) == len(wf_dict_AWG_list):
         raise RuntimeError("Inputs wf_dict_AWG_list and N of meas_AWG_SMU have different lengths.")
     
@@ -343,28 +351,39 @@ def save_meas(meas_dict, key):
     with open(f"meas_dict/{key}_{date.today()}.json", "w") as json_file:
             json_file.write(json.dumps(meas_dict))
 
-def get_params(P, R):
-    b = np.where(np.array(P) == N)[0][0]
-    x_s = P[:b+1]
-    x_r = P[b:]
-    y_s = 1/np.array(R[:101])
-    y_r = 1/np.array(R[100:])
-    A = min(y_s.min(), y_r.min())
-    y_s /= A
-    y_r /= A
-    y_s -= 1
-    y_r -= 1
-    B = max(y_s.max(), y_r.max())
-    y_s /= B
-    y_r /= B
-    plt.plot(y_s, x_s, '.b')
-    plt.plot(y_r, x_r, '.r')
-    popt_s, pcov_s = curve_fit(pulse_s, y_s, x_s, [1, 1e-2])
-    popt_r, pcov_r = curve_fit(pulse_r, y_r, x_r, [1, 1e-2])
-    plt.plot(y_s, pulse_s(y_s, *popt_s), '-b')
-    plt.plot(y_r, pulse_r(y_r, *popt_r), '-r')
-    plt.show()
+def get_params(P, R, plot=True):
+    points_s = np.where(np.array(P) > 0)[0]
+    points_r = np.where(np.array(P) < 0)[0]
+    y_s = P[points_s]
+    y_r = P[points_r]+N
+    x_s = 1/np.array(R[points_s])
+    x_r = 1/np.array(R[points_r])
+    A = min(x_s.min(), x_r.min())
+    x_s /= A
+    x_r /= A
+    x_s -= 1
+    x_r -= 1
+    B = max(x_s.max(), x_r.max())
+    x_s /= B
+    x_r /= B
+    x_s *= ss
+    x_r *= ss
+    x_s -= (ss-1)/2
+    x_r -= (ss-1)/2
+    
+    points_s = np.where(np.logical_and(np.array(x_s) >= 0, np.array(x_s) <= 1))[0]
+    points_r = np.where(np.logical_and(np.array(x_r) >= 0, np.array(x_r) <= 1))[0]
+    popt_s, pcov_s = curve_fit(pulse_s, x_s[points_s], y_s[points_s], [1, 1e-2])
+    popt_r, pcov_r = curve_fit(pulse_r, x_r[points_r], y_r[points_r], [1, 1e-2])
+    x = np.linspace(0, 1, 101)
+    if plot:
+        plt.plot(x_s, y_s, '.b')
+        plt.plot(x_r, y_r, '.r')
+        plt.plot(x, pulse_s(x, *popt_s), '-b')
+        plt.plot(x, pulse_r(x, *popt_r), '-r')
+        plt.show()
     params = {
+        'N' : N,
         'A' : A,
         'B' : B,
         'popt' : {
@@ -382,60 +401,77 @@ def pulse_r(G, alfa, beta):
     G_inf = 1-1/(1-np.exp(-beta*N**alfa))
     return N -(-np.log(1-(1-G)/(1-G_inf))/beta)**(1/alfa)
 
-def pulse_n(G_i, G_f, popt):
+def pulse_n(G_c, G_n, popt):
+    G_i = np.clip(G_c, 0, 1)
+    G_f = np.clip(G_n, 0, 1)
     if G_f > G_i:
         return pulse_s(G_f, *popt['s']) - pulse_s(G_i, *popt['s'])
     else:
         return pulse_r(G_f, *popt['r']) - pulse_r(G_i, *popt['r'])
 
-def characterize(n):
+def characterize(n, V_lim):
+    V = [*np.linspace(V_lim[0], V_lim[1], N), *np.linspace(V_lim[2], V_lim[3], N)]
     wf_dict_AWG = {
-        'V' : [2.4, -2.6], # pulse voltages
-        'n' : [N, N],   # pulse repetitions
-        'T' : 1e-3,     # pulse period
-        'W' : 1e-4,     # pulse width
+        'V' : V, # pulse voltages
+        'n' : [1 for i in range(N*2)],   # pulse repetitions
+        'W' : [2e-4 for i in range(N*2)],     # pulse widths
+        'T' : 2e-3,     # pulse period
         'read' : True,
-        'read_V' : .1,
-        'ch' : 1
+        'read_V' : read_V,
+        'ch' : ch_AWG
     }
     get_and_set_wf('AWG', wf_dict_AWG)
+    P = np.array([])
+    P_i  = np.concatenate([np.arange(N), np.arange(-1, -N-1, -1)])
+    R = []
     for i in range(n):
+        P = np.concatenate([P, P_i])
         results_AWG = meas_AWG(113, wf_dict_AWG, [1, 2], R_s=1e3, R_min=5e3, plot = False)
-    return np.concatenate([np.arange(N+1), np.arange(N-1, -1, -1)]), results_AWG['R_mean']
+        R.extend(results_AWG['R_mean'])
+    return P, np.array(R)
     
 def read(params, ch_DAQ_AWG, ch_DAQ_SMU):
     wf_dict_SMU = {
         'V' : [0],
         'n' : [0],
+        'W' : [0],
         'T' : 1,
-        'W' : 0,
         'read' : True,
-        'read_V' : .1,
-        'ch' : 1
+        'read_V' : read_V,
+        'ch' : ch_SMU
     }
     results = meas_AWG_SMU([], [], ch_DAQ_AWG, ch_DAQ_SMU, wf_dict_SMU, R_s = 1e3, plot=False)
-    return (results['R'][0]**-1/params['A'] - 1)/params['B']
+    return (results['R'][0]**-1/params['A'] - 1)/params['B'] * ss - (ss-1)/2
 
-def write(params, ch_DAQ_AWG, ch_DAQ_SMU, G):
+def write(params, ch_DAQ_AWG, ch_DAQ_SMU, G, V_lim):
     G_i = np.clip(read(params, ch_DAQ_AWG, ch_DAQ_SMU), 0, 1)
     G_f = np.clip(G, 0, 1)
-    
     n = pulse_n(G_i, G_f, params['popt'])
-    wf_dict_AWG = {
-        'V' : [2.4 if n>0 else -2.6], # pulse voltages
-        'n' : [round(abs(n))],   # pulse repetitions
-        'T' : 1e-3,     # pulse period
-        'W' : 1e-4,     # pulse width
-        'read' : False,
-        'read_V' : 0,
-        'ch' : 1
-    }
-    get_and_set_wf('AWG', wf_dict_AWG)
-    DAQ = instruments['DAQ']
-    AWG = instruments['AWG']
-    ch_AWG =  wf_dict_AWG['ch']
-    DAQ.set_conn(ch_DAQ_AWG)
-    AWG.set_outp(ch_AWG, 1)
-    AWG.trigger()
-    AWG.set_outp(ch_AWG, 0)
+    
+    if n > 0:
+        i0, i1, g0, g1 = 0, 1, G_i, G_f
+    else:
+        i0, i1, g0, g1 = 2, 3, 1-G_i, 1-G_f
+    n = abs(round(n))
+    dV = V_lim[i1] - V_lim[i0]
+    V = np.linspace(dV*g0, dV*g1, n) + V_lim[i0]
+    if n > 0:
+        wf_dict_AWG = {
+            #'V' : [2.4 if n>0 else -2.6], # pulse voltages
+            #'n' : [round(abs(n))],   # pulse repetitions
+            'V' : V, # pulse voltages
+            'n' : [1 for i in range(n)],   # pulse repetitions
+            'W' : [2e-4 for i in range(n)],     # pulse widths
+            'T' : 2e-3,     # pulse period
+            'read' : False,
+            'read_V' : 0,
+            'ch' : ch_AWG
+        }
+        get_and_set_wf('AWG', wf_dict_AWG)
+        DAQ = instruments['DAQ']
+        AWG = instruments['AWG']
+        DAQ.set_conn(ch_DAQ_AWG)
+        AWG.set_outp(ch_AWG, 1)
+        AWG.trigger()
+        AWG.set_outp(ch_AWG, 0)
     
